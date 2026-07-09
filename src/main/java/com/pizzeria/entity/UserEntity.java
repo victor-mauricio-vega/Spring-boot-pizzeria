009e18b50934c97576b048e7bbcaad679f4efab6
Victor Mauricio Vega <v.mauriciovega.m@gmail.com>
2026-07-09 13:10:51 -0500

    refactor(config): Se realiza configuracion para variables de entorno

---
+++ src/main/java/com/pizzeria/entity/UserEntity.java
@@ -0,0 +1,42 @@
+package com.pizzeria.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_user;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, name = "nombres")
+    private String name;
+
+    @Column(nullable = false, name = "apellidos")
+    private String lastname;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private  LocalDateTime updateAt;
+
+}
